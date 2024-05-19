fabc07529482d482c26fc9a1228a5c53a177eba0
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-20 00:02:02 +0900

    Update 자릿수 더하기.py

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,6 +1,23 @@
+# My Code
 def solution(n):
     n = str(n)
     answer = 0
     for x in n:
         answer += int(x)
+    return answer
+
+# Best Code(Simpler Ver.)
+def solution(n):
+    return sum(int(i) for i in str(n))
+
+'''
+    내 코드랑 비슷하지만 조금 더 간결해보인다!
+'''
+
+# Another Code 
+def solution(n):
+    answer = 0
+    while n:
+        answer += n%10
+        n //= 10
     return answer
