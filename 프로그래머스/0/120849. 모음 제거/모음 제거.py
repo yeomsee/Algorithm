ca4641e9424fbc01327dc806bdc8b9d7021fa200
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-20 00:35:29 +0900

    Update 모음 제거.py

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,3 +1,4 @@
+# My Code
 def solution(my_string):
     vowel_list = ['a', 'e', 'i', 'o', 'u']
     answer = ''
@@ -5,3 +6,14 @@ def solution(my_string):
         if string not in vowel_list:
             answer += string
     return answer
+
+# Best Code(Simpler Ver.)
+def solution(my_string):
+    vowels = ['a','e','i','o','u']
+    for vowel in vowels:
+        my_string = my_string.replace(vowel, '')
+    return my_string
+
+# Another Code
+def solution(my_string):
+    return "".join([i for i in my_string if not(i in "aeiou")])
