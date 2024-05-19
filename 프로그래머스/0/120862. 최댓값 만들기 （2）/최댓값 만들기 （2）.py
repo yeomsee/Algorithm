f42dab24770a56f0d01116d72c97be49aa2b0dfb
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-20 00:26:35 +0900

    Update 최댓값 만들기 （2）.py

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,6 +1,21 @@
+# My Code
 def solution(numbers):
     numbers.sort()
     x1, x2, x3, x4 = numbers[0], numbers[1], numbers[-1], numbers[-2]
     answer = x1*x2 if x1*x2 > x3*x4 else x3*x4
     
     return answer
+
+# Best Code (Similar Ver.)
+def solution(numbers):
+    numbers.sort()
+    return max(numbers[0] * numbers[1], numbers[-1] * numbers[-2])
+
+# Another Code
+from itertools  import combinations as comb
+
+def solution(numbers):
+    an_list=[]
+    for i,j in comb(numbers,2):
+        an_list.append(i*j)
+    return max(an_list)
