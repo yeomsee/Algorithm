67bb557b93288562132f9a683c9f7d2f73112d36
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-19 17:49:33 +0900

    Update 가위 바위 보.py

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,3 +1,4 @@
+# My Code
 def solution(rsp):
     win_dict = {'2' : '0', '0' : '5', '5' : '2'}
     
@@ -6,3 +7,8 @@ def solution(rsp):
         answer += win_dict[string]
         
     return answer
+
+# Best Code
+def solution(rsp):
+    d = {'0':'5','2':'0','5':'2'}
+    return ''.join(d[i] for i in rsp)
