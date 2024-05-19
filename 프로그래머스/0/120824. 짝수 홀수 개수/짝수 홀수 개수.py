1483ef2a1a4d84f6e52bf0651be2e5e9c4fd8094
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-19 17:42:39 +0900

    Update 짝수 홀수 개수.py

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,3 +1,4 @@
+# My Code
 def solution(num_list):
     answer = []
     even_count, odd_count = 0, 0
@@ -9,3 +10,15 @@ def solution(num_list):
     answer.insert(0, even_count)
     answer.insert(1, odd_count)
     return answer
+
+# Best Code
+def solution(num_list):
+    answer = [0, 0]
+    for n in num_list:
+        answer[n%2]+=1
+    return answer
+
+# Best Code 2
+def solution(num_list):
+    odd = sum(1 for n in num_list if n % 2)
+    return [len(num_list) - odd, odd]
