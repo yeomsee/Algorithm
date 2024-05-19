6eccced20a4be31b5fe40db014b372215cbb17ff
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-19 17:32:52 +0900

    Update 머쓱이보다 키 큰 사람.py

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -7,3 +7,10 @@ def solution(array, height):
 # Similar Code
 def solution(array, height):
     return sum(1 for a in array if a > height)
+
+# Best Code
+def solution(array, height):
+    array.append(height)
+    array.sort(reverse=True)
+
+    return array.index(height)
