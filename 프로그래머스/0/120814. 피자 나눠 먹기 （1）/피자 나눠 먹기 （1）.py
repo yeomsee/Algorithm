2b9d05a9e1584068691c8b52aa15ba5980c77742
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-19 17:43:34 +0900

    Update 피자 나눠 먹기 （1）.py

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,6 +1,11 @@
+# My Code
 def solution(n):
     if n%7 == 0:
         answer = n//7
     else:
         answer = n//7 + 1
     return answer
+
+# Best Code
+def solution(n):
+    return (n - 1) // 7 + 1
