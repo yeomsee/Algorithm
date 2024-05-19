7cc0e219114a278822eb69bc94999f81e49aed49
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-19 20:13:07 +0900

    Update 암호 해독.py

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,6 +1,17 @@
+# My Code
 def solution(cipher, code):
     answer = ''
     for i in range(code-1, len(cipher), code):
         answer += cipher[i]
         
     return answer
+
+# Best Code(Simpler Ver.)
+def solution(cipher, code):
+    answer = cipher[code-1::code]
+    return answer
+
+'''
+    list slicing에 대한 이해가 부족했던 듯,,
+    좀 더 간단하고 직관적이게 짜보려고 노력하자!
+'''
