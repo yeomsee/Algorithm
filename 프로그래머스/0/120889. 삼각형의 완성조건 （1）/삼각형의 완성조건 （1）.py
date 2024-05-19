cbf5a14200ddca59b755d53721c47b5af429ba8a
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-19 20:04:38 +0900

    Update 삼각형의 완성조건 （1）.py

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,10 +1,21 @@
+# My Code
 def solution(sides):
     sides.sort()
-    s, m, l = sides[0], sides[1], sides[2]
+    s, m, l = sides[0], sides[1], sides[2] # small, medium, large
     
-    if l < (s+m):
+    if l < (s+m): # 삼각형 형성 조건
         answer = 1
     else:
         answer = 2
         
     return answer
+
+# Another Code : Simpler Code
+def solution(sides):
+    sides.sort()
+    return 1 if sides[0]+sides[1]>sides[2] else 2
+
+# Best Code
+def solution(sides):
+    return 1 if max(sides) < (sum(sides) - max(sides)) else 2
+
