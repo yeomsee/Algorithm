00becd94045fa6517237f987f067a6ac081b2081
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-19 18:39:48 +0900

    Update 숨어있는 숫자의 덧셈 （1）.py

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,3 +1,12 @@
+# My Code
 def solution(my_string):
     answer = sum([int(x) for x in my_string if ord("1") <= ord(x) <= ord("9")])
     return answer
+
+# Another Code
+def solution(my_string):
+    return sum(int(i) for i in my_string if i.isdigit())
+
+'''
+    str.isdigit() : str이 0 이상의 정수로만 이루어진 문자열에서만 True를 반환
+'''
