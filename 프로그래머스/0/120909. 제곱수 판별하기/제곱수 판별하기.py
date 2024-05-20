5215f95882a7c0a4ec8d09754d213ccf38c55be7
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-21 01:16:39 +0900

    Update 제곱수 판별하기.py

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,3 +1,16 @@
+# My code
 def solution(n):
     answer = 1 if (int(n**0.5))**2 == n else 2
     return answer
+
+'''
+    ** 연산자 인자에 1/2이 들어가도 괜찮음!
+'''
+
+# Another Code
+def solution(n):
+    return 1 if (n ** 0.5).is_integer() else 2
+
+'''
+    is_integer() 함수를 이용
+'''
