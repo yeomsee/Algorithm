2f24e960c689c04bc0cb6067efd5fea0d212d203
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-21 01:23:48 +0900

    Update 점의 위치 구하기.py

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,3 +1,4 @@
+# My Code
 def solution(dot):
     x, y = dot[0], dot[1]
     if x > 0:
@@ -11,3 +12,17 @@ def solution(dot):
         elif y < 0:
             answer = 3
     return answer
+
+# Another Code(Simpler Ver.)
+def solution(dot):
+    x,y = dot
+    if x*y>0:
+        return 1 if x>0 else 3
+    else:
+        return 4 if x>0 else 2
+
+'''
+    x, y = dot[0], dot[1]
+    x, y = dot
+    => 같은 결과!
+'''
