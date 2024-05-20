f17823bf773041e4b04267f480b2850832e1bd50
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-05-21 01:44:56 +0900

    Update 순서쌍의 개수.py

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,3 +1,4 @@
+# My code
 def solution(n):
     answer = []
     for i in range(1, n+1):
@@ -5,3 +6,11 @@ def solution(n):
             answer.append((i, n*i))
             
     return len(answer)
+
+# Another Code (Simpler Ver.)
+def solution(n):
+    return len([number for number in range(1, n+1) if n%number == 0])
+
+'''
+    어차피 갯수만 세면 되는 거니까 굳이 append까지 할 필요는 없었을 듯
+'''
