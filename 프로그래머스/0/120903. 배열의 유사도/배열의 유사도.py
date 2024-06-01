8ce9ea4549860ada35af7c28cc80d3539dd3c418
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-01 22:47:15 +0900

    Update 배열의 유사도.py

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,3 +1,8 @@
+# My Code
 def solution(s1, s2):
     answer = sum([1 for w1 in s1 if w1 in s2])
     return answer
+
+# Best Code : set을 이용한 효율적인 처리
+def solution(s1, s2):
+    return len(set(s1)&set(s2))
