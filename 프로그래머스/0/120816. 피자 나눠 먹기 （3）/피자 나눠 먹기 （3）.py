649ddca66c820f42baef828033deff4c30c79f2e
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-01 23:04:42 +0900

    Update 피자 나눠 먹기 （3）.py

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,3 +1,12 @@
+# My Code
 def solution(slice, n):
     answer = n // slice if n % slice == 0 else n // slice + 1
     return answer
+
+# Best Code : Simpler Ver.
+def solution(slice, n):
+    return ((n - 1) // slice) + 1
+
+'''
+    지난 번에도 봤던 코드인 거 같은데 아직 익숙해지지 않았다ㅠㅠ
+'''
