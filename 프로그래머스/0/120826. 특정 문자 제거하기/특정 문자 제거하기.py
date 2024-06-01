a8991facae42ddd8ed631c26e0c4b568e88a274b
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-01 22:58:56 +0900

    Update 특정 문자 제거하기.py

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,3 +1,20 @@
+# My Code = Best Code
 def solution(my_string, letter):
     answer = my_string.replace(letter, '')
     return answer
+
+# Another Code1 : replace 사용 X
+def solution(my_string, letter):
+    answer = ''
+    for string in my_string:
+        if string != letter:
+            answer += string
+    return answer
+
+'''
+    원래는 이걸로 하려고했지만 좀 더 간결하고 직관적인 코드를 작성하고 싶었음!
+'''
+
+# Another Code2 : join 활용
+def solution(my_string, letter):
+    return ''.join([c for c in my_string if c != letter])
