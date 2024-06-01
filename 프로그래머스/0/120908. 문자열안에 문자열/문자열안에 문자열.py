d43758f9c1085679d9c7b3eaada2b8d3482f277b
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-01 23:46:18 +0900

    Update 문자열안에 문자열.py

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -11,3 +11,16 @@ def solution(str1, str2):
                 if j == (len(str2) - 1):
                     return 1
     return 2
+
+'''
+    자료구조 때 배웠던 String Matching의 Naive Version을 생각하면서 구현해봤다.
+    파이썬 스타일이라기보다는 오히려 C, C++ 느낌이 나는 거 같다.
+'''
+
+# Best Code
+def solution(str1, str2):
+    return 1 if str2 in str1 else 2
+
+'''
+    사실 처음에는 이걸로 했는데 Error가 떠서 포기했었는데 후,, 오타가 있었나보다
+'''
