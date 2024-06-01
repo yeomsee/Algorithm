537173808346261f20a12e53880942b87b14bca5
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-01 23:52:57 +0900

    Update 문자열 뒤집기.py

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -2,3 +2,11 @@
 def solution(my_string):
     answer = ''.join([my_string[i] for i in range(len(my_string)-1, -1, -1)])
     return answer
+
+# Best Code
+def solution(my_string):
+    return my_string[::-1]
+
+'''
+    지난 번에 봤던 코드,, 다시 한 번 잘 새기자
+'''
