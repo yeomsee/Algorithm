2ae2f70d5ebab78883a8bec86fe178dceb29b779
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-01 22:51:35 +0900

    Update 문자 반복 출력하기.py

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,6 +1,15 @@
+# My Code
 def solution(my_string, n):
     answer = ''
     for string in my_string:
         answer += string*n
     
     return answer
+
+# Best Code
+def solution(my_string, n):
+    return ''.join(i*n for i in my_string)
+
+'''
+    join을 활용해서 효율적으로 만들 수도 있음.
+'''
