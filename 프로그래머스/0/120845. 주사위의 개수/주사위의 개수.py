1c1803d776636f3e11b01c4336a42a63a22ab56f
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-03 01:18:28 +0900

    Update 주사위의 개수.py

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,6 +1,6 @@
 # My Code
 def solution(box, n):
-    x, y, z = box[0], box[1], box[2]
+    x, y, z = box[0], box[1], box[2] # x, y, z = box
     
     answer = (x//n) * (y//n) * (z//n)                
     return answer
