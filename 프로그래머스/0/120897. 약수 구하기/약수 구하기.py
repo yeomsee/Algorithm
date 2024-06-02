ca87c7b4f2faeccf264173041082afcd24409456
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-02 17:39:21 +0900

    Update 약수 구하기.py

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,7 +1,23 @@
-# My Code .. 시간복잡도는 꽤 있는 듯
+# My Code .. O(n)
 def solution(n):
     answer = []
     for i in range(1, n+1):
         if n%i == 0:
             answer.append(i)
     return answer
+
+# Best Code .. O(root(n))
+def find(n):
+    answer = []
+    for i in range(1, int(i**0.5) + 1):
+        # n의 제곱근까지만 loop를 돌면서 나누어떨어지는 것들을 append
+        if n % i == 0:
+            answer.append(i)
+            # i와 쌍을 이루는 약수들도 추가
+            if i**2 != n:
+                answer.append(n // i)
+    return answer
+
+def solution(n):
+    answer = find(n)
+    return sorted(answer)
