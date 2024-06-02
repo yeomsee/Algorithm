0be11f0bd882d59da0f2f1c72cf73058fe2ac2b3
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-02 17:26:13 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,10 @@
+# My Code
+def solution(numbers, direction):
+    answer = []
+    for i in range(len(numbers)):
+        if direction == "left":
+            answer.append(numbers[(i+1) % len(numbers)])
+        else:
+            answer.append(numbers[(i+len(numbers)-1) % len(numbers)])
+            
+    return answer
