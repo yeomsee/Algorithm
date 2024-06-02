377e89d2d9ae39d5a8e47d26cb493e14743f9667
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-03 01:09:20 +0900

    Update 인덱스 바꾸기.py

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -10,3 +10,19 @@ def solution(my_string, num1, num2):
         else:
             answer += my_string[i]
     return answer
+
+# Best Code
+def solution(my_string, num1, num2):
+    s = list(my_string)
+    s[num1],s[num2] = s[num2],s[num1]
+    return ''.join(s)
+
+'''
+    Eg.
+    my_string = 'python'
+    list(my_string) = ['p', 'y', 't', 'h', 'o', 'n']
+    
+    처음에는 my_string[num1], my_string[num2] = my_string[num2], my_string[num1]
+    But, 'str' object does not support item assignment
+    때문에 위처럼 리스트로 바꿔서 실행
+'''
