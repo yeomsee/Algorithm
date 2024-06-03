8bb35db2e7e281036fd9ea4662085582d3c7c111
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-04 00:42:08 +0900

    Update 문자열 정렬하기 （2）.py

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -4,3 +4,17 @@ def solution(my_string):
     my_string.sort()
     
     return ''.join(my_string)
+
+# Another Code
+def solution(my_string):
+    answer = []
+    for i in my_string:
+        if ord(i) >= ord('A') and ord(i) <= ord('Z'):
+            answer.append(chr(ord(i)+32))
+        else:
+            answer.append(i)
+    return ''.join(sorted(answer))
+
+'''
+    한줄짜리 코드도 좋지만 이렇게 아스키 코드를 이용해서 풀줄도 알아야할 거 같다.
+'''
