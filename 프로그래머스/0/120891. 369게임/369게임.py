2459f7ff51e95f236306203f6d254d552b9d13cb
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-06-04 01:19:50 +0900

    Update 369게임.py

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -7,3 +7,8 @@ def solution(order):
         if num == "3" or num == "6" or num == "9":
             answer += 1
     return answer
+
+'''
+    원래는 if int(num)%3 == 0 이라고 썼는데 num이 "0"인 경우는 고려하지 않았다.
+    int(num) != 0 까지 썼으면 돌아갔을 듯.
+'''
