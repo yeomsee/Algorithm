8f7030eccb90c90e651fe92bc2b000af4eb798ae
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-07-22 00:36:12 +0900

    Update 합성수 찾기.py

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -10,3 +10,19 @@ def solution(n):
             answer_list.append(x)
             
     return len(answer_list)
+
+# Best Code
+def solution(n):
+    output = 0
+    for i in range(4, n + 1): # 4부터 모든 수에 대해서 반복
+        for j in range(2, int(i ** 0.5) + 1): # 2부터 i의 제곱근까지의 수에 대해서만 반복
+            if i % j == 0: # i가 합성수라면
+                output += 1
+                break # 더 이상 수행할 필요가 없음
+    return output
+
+
+'''
+    사실 'NOT 소수 = 합성수'이므로
+    내 풀이처럼 count를 굳이 안 써도 됐을 거 같긴 하다,,,
+'''
