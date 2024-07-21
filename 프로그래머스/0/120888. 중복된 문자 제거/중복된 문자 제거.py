9c282c083f7679d912321a6043204e5800e62d3a
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-07-22 00:55:50 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,11 +1,7 @@
 # My Code
 def solution(my_string):
-    answer_list = []
+    answer = ''
     for string in my_string:
-        if len(answer_list) == 0:
-            answer_list.append(string)
-        else:
-            if string not in answer_list:
-                answer_list.append(string)
-    answer = ''.join(answer_list)
+        if string not in answer:
+            answer += string
     return answer
