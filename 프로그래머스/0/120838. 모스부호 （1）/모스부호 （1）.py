eaf7513d0f3e0ed14dea53d13c5240be497174c3
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-07-22 01:28:29 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,16 @@
+# My Code
+def solution(letter):
+    morse_dict = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    
+    answer = ''
+    letter_list = letter.split(' ')
+    for morse in letter_list:
+        answer += morse_dict[morse]
+    
+    return answer
