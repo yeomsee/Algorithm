59dac4e56b495ccdefcdd7c5a98489b49e2ff7c0
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-07-22 01:21:07 +0900

    Update 진료 순서 정하기.py

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -6,3 +6,8 @@ def solution(emergency):
         index = sorted_emergency.index(num) # 각 value가 정렬된 리스트에서 몇 번째인지?
         answer_list.append(index+1)
     return answer_list
+
+# Simpler Code
+def solution(emergency):
+    e = sorted(emergency, reverse=True) # 'list(reversed(sorted(emergency)))'에 비해서 훨씬 간단한 방법
+    return [e.index(i)+1 for i in emergency]
