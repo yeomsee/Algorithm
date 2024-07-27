af2bc3f9f9a276534ec80e0c6ae23c6b94d8e9ab
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-07-28 00:06:31 +0900

    Update A로 B 만들기.py

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -7,3 +7,17 @@ def solution(before, after):
     if before == after:
         return 1
     return 0
+
+# Simpler Code
+'''
+    굳이 list로 변환할 필요가 있었을까?
+    sort() 함수는 리시트에서만 먹힌다고 생각했어서 그랬,,
+'''
+
+def solution(before, after):
+    before=sorted(before)
+    after=sorted(after)
+    if before==after:
+        return 1
+    else:
+        return 0
