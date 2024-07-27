7e78e31733d330ee7f404cd44380c2074eede1e7
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-07-27 23:59:39 +0900

    Update 숨어있는 숫자의 덧셈 （2）.py

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -11,3 +11,19 @@ def solution(my_string):
     for num in answer.split():
         sum += int(num)
     return sum
+
+# Simpler Code
+def solution(my_string):
+    s = ''.join(i if i.isdigit() else ' ' for i in my_string)
+    return sum(int(i) for i in s.split())
+
+# 정규표현식 사용하기
+import re
+
+def solution(my_string):
+    return sum([int(i) for i in re.findall(r'[0-9]+', my_string)])
+
+'''
+    re.findall(r'[0-9]+', my_string) 설명
+    my_string에서 r'[0-9]+'를 통해 하나 이상의 연속된 숫자를 찾음.
+'''
