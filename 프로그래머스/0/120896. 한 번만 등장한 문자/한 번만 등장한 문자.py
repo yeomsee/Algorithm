5fe574b50be762e0b8af3b2203cfffc27b055b4d
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-07-28 23:01:01 +0900

    Update 한 번만 등장한 문자.py

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -8,3 +8,8 @@ def solution(s):
             answer += string
             
     return answer
+
+# Simpler Code
+def solution(s):
+    answer = "".join(sorted([ch for ch in s if s.count(ch) == 1]))
+    return answer
