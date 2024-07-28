81f76125e5ebf2e3683aeebe545f10681289ec4d
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-07-28 22:50:52 +0900

    Update 가까운 수.py

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -11,3 +11,19 @@ def solution(array, n):
         else:
             answer_list.append(array[i])
     return min(answer_list)
+
+# Best Code
+def solution(array, n):
+    array.sort()
+    temp = []
+
+    for i in array :
+        temp.append( abs(n-i) )
+
+    return array[temp.index(min(temp))]
+
+'''
+    이거 이외에도 한 줄짜리 코드가 있기는 한데,,
+    사실 그렇게 코드를 작성하는 게 직관적일까? 라는 생각이 들어서
+    어느 정도 길이가 있는 직관적인 코드를 들고 오게 됨.
+'''
