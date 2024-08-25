b3f6a037e8478112e23b821c442becedd159e5e2
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-08-26 02:38:35 +0900

    [level 0] Title: 소인수분해, Time: 1.23 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,20 @@
+# My Code
+def solution(n):
+    # 소수인지를 체크하는 함수
+    def check_prime_num(num):
+        count = 0
+        for x in range(2, num+1):
+            if num%x == 0:
+                count += 1
+            if count >= 2:
+                return False
+        return True
+    
+    answer_list = []
+    for i in range(2, n+1):
+        # 약수인 경우 + 그 약수가 소수인 경우 answer_list에 append
+        if n%i == 0 and check_prime_num(i):
+            answer_list.append(i)
+    answer_list.sort()
+    
+    return answer_list
