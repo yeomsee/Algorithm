2a435a4bce381cb51d2b827551dfd7d0aa07d760
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-08-30 19:01:22 +0900

    Update 두 수의 연산값 비교하기.py

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,4 +1,7 @@
 # My Code
 def solution(a, b):
     return hap if (hap := int(str(a) + str(b))) > (gob := 2*a*b) else gob
-    
+
+# Simpler Code
+def solution(a, b):
+    return max(int(str(a) + str(b)), 2 * a * b)
