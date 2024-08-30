896460f8cb16014a293eedb01bc2b5a9d113e6c8
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-08-30 19:09:20 +0900

    Update 배열의 원소 삭제하기.py

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -4,3 +4,7 @@ def solution(arr, delete_list):
         if num2del in arr:
             arr.remove(num2del)
     return arr
+
+# Simpler Code
+def solution(arr, delete_list):
+    return [i for i in arr if i not in delete_list]
