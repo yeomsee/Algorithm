60f338f12a66c2976b9c310b54ca121a974ad769
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-08-30 17:36:01 +0900

    Update 길이에 따른 연산.py

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -8,3 +8,24 @@ def solution(num_list):
 
     answer = sum(num_list) if len(num_list) >= 11 else multiply(num_list)
     return answer
+
+# Best Code
+def solution(num_list):
+    if len(num_list) >= 11:
+        return eval('+'.join(list(map(str, num_list))))
+    else:
+        return eval('*'.join(list(map(str, num_list))))
+
+'''
+    * map(function, iterable)
+    : iterable한 객체 안에 있는 모든 요소들에 대해 function을 적용
+
+    * eval(expression : str)
+    : expression을 받아서 파이썬이 알아서 실행해주는 함수
+'''
+
+# Another Code
+from math import prod
+
+def solution(num_list):
+    return sum(num_list) if len(num_list)>=11 else prod(num_list)
