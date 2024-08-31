9588fedf564c111667ca807f115cebf3cba91733
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-01 02:40:54 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,15 @@
+# My Code
+def solution(arr1, arr2):
+    if len(arr1) > len(arr2):
+        return 1
+    elif len(arr1) < len(arr2):
+        return -1
+    else:
+        if sum(arr1) > sum(arr2):
+            return 1
+        elif sum(arr1) < sum(arr2):
+            return -1
+        else:
+            return 0
+        
+    return answer
