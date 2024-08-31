6623f665a9c0f27ecd8522662a6897952c1d765d
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-01 01:20:13 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 9.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -0,0 +1,8 @@
+# My Code
+def solution(a, b):
+    if a%2 != 0 and b%2 != 0:
+        return a**2 + b**2
+    elif a%2 == 0 and b%2 == 0:
+        return abs(a - b)
+    else:
+        return 2*(a + b)
