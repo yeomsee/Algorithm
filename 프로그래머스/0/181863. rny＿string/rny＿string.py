10331c0170b4877f830a2bc2bb0ce187a2b7f1b2
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-01 02:43:48 +0900

    Update rny＿string.py

--- 프로그래머스/0/181863. rny＿string/rny＿string.py
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -1,3 +1,11 @@
 # My Code
 def solution(rny_string):
     return ''.join(['rn' if string == 'm' else string for string in rny_string])
+
+# Best Code
+def solution(rny_string):
+    return rny_string.replace('m', 'rn')
+
+'''
+    replace 함수를 이용한 깔끔한 풀이,,!
+'''
