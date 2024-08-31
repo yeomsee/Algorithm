c737024d1be368a8b4cef5ba1c2b4aa4b0d2dd3d
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-01 01:10:45 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.41 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -0,0 +1,9 @@
+# My Code
+def solution(arr, n):
+    for i in range(len(arr)):
+        if len(arr)%2 != 0 and i%2 == 0:
+            arr[i] += n
+        elif len(arr)%2 == 0 and i%2 != 0:
+            arr[i] += n
+        
+    return arr
