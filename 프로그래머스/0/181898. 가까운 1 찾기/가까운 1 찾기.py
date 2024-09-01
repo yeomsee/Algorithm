d10924beb2f9b8d8a4950b5da70c4240de139b96
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-01 15:50:45 +0900

    Update 가까운 1 찾기.py

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -4,3 +4,17 @@ def solution(arr, idx):
         if arr[i] and i >= idx:
             return i
     return -1
+
+# Best Code
+def solution(arr, idx):
+    answer = 0
+    try:
+        answer = arr.index(1, idx)
+    except:
+        answer = -1
+
+    return answer
+
+'''
+    list.index(index, start)
+'''
