a2e3834b067b2e75ec9ffd05e7b3f6c91b82cc90
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-01 16:27:14 +0900

    Update 특별한 이차원 배열 2.py

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -5,3 +5,29 @@ def solution(arr):
             if arr[i][j] != arr[j][i]:
                 return 0
     return 1
+
+# Best Code
+def solution(arr):
+    return int(arr == list(map(list, zip(*arr))))
+
+'''
+    * zip 기본 문법
+    >>> numbers = [1, 2, 3]
+    >>> letters = ["A", "B", "C"]
+    >>> for pair in zip(numbers, letters):
+    ...     print(pair)
+    ...
+    (1, 'A')
+    (2, 'B')
+    (3, 'C')
+
+    * zip 병렬 처리
+    >>> for number, upper, lower in zip("12345", "ABCDE", "abcde"):
+        ...     print(number, upper, lower)
+        ...
+        1 A a
+        2 B b
+        3 C c
+        4 D d
+        5 E e
+'''
