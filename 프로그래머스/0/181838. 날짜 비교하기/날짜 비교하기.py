e37327b48a4c68edeede8d07782b0828f09f118f
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-02 16:58:12 +0900

    Update 날짜 비교하기.py

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -8,3 +8,10 @@ def solution(date1, date2):
         else: # date1[i] == date2[i]
             if i == len(date1)-1:
                 return 0
+
+# Simpler Code
+def solution(date1, date2):
+    for i in range(3):
+        if date1[i]<date2[i]:return 1
+        elif date2[i]<date1[i]: return 0
+    return 0
