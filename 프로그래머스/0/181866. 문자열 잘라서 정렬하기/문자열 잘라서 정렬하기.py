d5ff660202d9bb1e755aa65ca9edca73d4e5bbd3
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-02 16:48:32 +0900

    Update 문자열 잘라서 정렬하기.py

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -4,3 +4,13 @@ def solution(myString):
     answer.sort()
     
     return answer
+
+# Simpler Code
+def solution(myString):
+    return sorted(ch for ch in myString.split('x') if ch)
+
+'''
+    if ch이면 공백인 문자는 제거할 수 있음
+
+    그리고 sorted의 경우는 애초에 반환이 리스트인 점 기억!
+'''
