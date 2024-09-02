42c6ad2f4a44a3f84f3593172a9e7ded73da1998
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-03 02:45:38 +0900

    Update 배열의 길이를 2의 거듭제곱으로 만들기.py

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.py
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.py
@@ -10,3 +10,14 @@ def solution(arr):
             surplus = 2**i - len(arr)
             arr.extend([0]*surplus)
             return arr
+
+# Simpler Code
+def solution(arr):
+    a = 1
+    b = len(arr)
+    while a < b :
+        a *= 2
+    return arr + [0] * (a-b)
+'''
+    논리는 비슷한데 훨씬 더 직관적이고 효율적인 코딩이라고 생각해서 들고왔습니다.
+'''
