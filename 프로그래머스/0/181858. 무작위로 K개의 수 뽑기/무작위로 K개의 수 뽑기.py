656437a4ae09b9e11766defaa501dcb78d9ebea4
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-14 01:59:28 +0900

    Update 무작위로 K개의 수 뽑기.py

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -9,4 +9,20 @@ def solution(arr, k):
         for _ in range(left):
             answer_list.append(-1)
     return answer_list
-    
+
+# Best Code
+def solution(arr, k):
+    ret = []
+    for i in arr:
+        if i not in ret:
+            ret.append(i)
+        if len(ret) == k:
+            break
+
+    return ret + [-1] * (k - len(ret))
+
+'''
+    내 풀이는 너무 경직돼서 푸는 느낌,,?
+    좀 유연하게 풀어보자!
+    생각! 생각! 생각!
+'''
