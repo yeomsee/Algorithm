cd32a6e191c0646c505356dcc0b7c5005db35979
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-16 16:46:50 +0900

    Update 직사각형 넓이 구하기.py

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -6,3 +6,7 @@ def solution(dots):
     x_m, x_M, y_m, y_M = min(x_list), max(x_list), min(y_list), max(y_list)
     
     return (x_M - x_m)*(y_M - y_m)
+
+# Best Code ... 수학적으로 생각해보기
+def solution(dots):
+    return (max(dots)[0] - min(dots)[0])*(max(dots)[1] - min(dots)[1])
