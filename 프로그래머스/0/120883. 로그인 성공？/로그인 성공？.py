78e65560b6f28b6be9ce824dd11aee725e41c194
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-17 16:33:33 +0900

    Update 로그인 성공？.py

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -8,3 +8,9 @@ def solution(id_pw, dbs):
             else:
                 return "wrong pw"
     return "fail"
+
+# Best Code
+def solution(id_pw, db):
+    if db_pw := dict(db).get(id_pw[0]):
+        return "login" if db_pw == id_pw[1] else "wrong pw"
+    return "fail"
