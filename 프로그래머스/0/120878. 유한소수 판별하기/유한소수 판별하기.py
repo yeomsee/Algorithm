fe376011760f17cc2675530439a5d8b6d45b6539
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-20 18:26:48 +0900

    Update 유한소수 판별하기.py

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -12,3 +12,18 @@ def solution(a, b):
         if (b%x == 0) and (x%2 != 0 and x%5 != 0):
             return 2
     return 1
+
+'''
+    기억하자!
+    파이썬에서 나누기 연산의 return 값은 무.조.건. 실수형이다!
+'''
+
+# Simpler Code
+from math import gcd
+def solution(a, b):
+    b //= gcd(a,b)
+    while b%2==0:
+        b//=2
+    while b%5==0:
+        b//=5
+    return 1 if b==1 else 2
