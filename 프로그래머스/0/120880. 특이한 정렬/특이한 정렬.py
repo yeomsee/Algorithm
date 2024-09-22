e36868847018e5fb2a584c3aae8405ac87476ac5
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-22 21:57:33 +0900

    Update 특이한 정렬.py

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -5,3 +5,13 @@ def solution(num_list, n):
     sorted_dis_num_list = sorted(dis_num_list, key = lambda x : x[0])
     
     return [x for _, x in sorted_dis_num_list]
+
+# Best Code
+def solution(numlist, n):
+    answer = sorted(numlist,key = lambda x : (abs(x-n), n-x))
+    return answer
+
+'''
+    key = lambda x : (abs(x-n), n-x) 라고 설정하면 
+    abs(x-n)가 첫 번째 기준이고 이 기준을 충족할 경우 두 번째 기준 (n-x)를 충족하는 구조
+'''
