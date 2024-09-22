eb225463f1314f0adb6e773445f3c2f91f7bb53e
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-22 23:09:08 +0900

    Update 배열 만들기 2.py

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -8,5 +8,15 @@ def solution(l, r):
     if len(answer) == 0:
         return [-1]
     return answer
-        
-    
+
+# Best Code
+def solution(l, r):
+    answer = []
+    for num in range(l, r + 1):
+        if not set(str(num)) - set(['0', '5']):
+            answer.append(num)
+    return answer if answer else [-1]
+
+'''
+    set을 이용해볼까도 당연히 생각봤는데,, 차집합의 개념을 이용하면 이렇게 깔끔하게 풀 수 있다는 것도 배웠다. 굿!
+'''
