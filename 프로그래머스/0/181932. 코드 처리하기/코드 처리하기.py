9372de6b0b7a572e1347e97e0ce7dcdbde35aa4a
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-27 18:30:19 +0900

    [level 0] Title: 코드 처리하기, Time: 15.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -4,7 +4,7 @@ def solution(code):
     for idx in range(len(code)):
         if code[idx] == "1":
             mode = (mode+1)%2
-        elif (mode == 0) and (idx%2 == 0) or (mode == 1) and (idx%2 != 0):
+        elif (mode == 0 and idx%2 == 0) or (mode == 1 and idx%2 != 0):
             ret += code[idx]
 
     if len(ret) == 0:
