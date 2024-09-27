e72840fcceff57fbe82623840c39294d8b37665d
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-27 18:34:05 +0900

    Update 코드 처리하기.py

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -10,3 +10,23 @@ def solution(code):
     if len(ret) == 0:
         return "EMPTY"
     return ret
+
+# Best Code
+def solution(code):
+    answer = ''
+
+    mode = 0
+    for i in range(len(code)):
+        if code[i] == '1':
+            mode ^= 1
+        else:
+            if i % 2 == mode:
+                answer += code[i]
+
+    return answer if answer else 'EMPTY'
+
+'''
+    많이 쓰일 것 같지는 않지만,, '^=' 연산자에 대하여,,
+    ^=은 xor을 뜻함
+    두 비트가 서로 같다면 0을 서로 다르다면 1을 return
+'''
