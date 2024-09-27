ad093ca757b7a7424f1fc9e0343f9faaddb1a6e4
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-27 18:29:53 +0900

    [level 0] Title: 코드 처리하기, Time: 19.31 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,12 @@
+# My Code
+def solution(code):
+    mode, ret = 0, ''
+    for idx in range(len(code)):
+        if code[idx] == "1":
+            mode = (mode+1)%2
+        elif (mode == 0) and (idx%2 == 0) or (mode == 1) and (idx%2 != 0):
+            ret += code[idx]
+
+    if len(ret) == 0:
+        return "EMPTY"
+    return ret
