5332329ba852ae29d321363507f5e6bb162df29d
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-28 19:32:37 +0900

    Update OX퀴즈.py

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -9,3 +9,22 @@ def solution(quiz):
             answer.append("X")
 
     return answer
+
+# Best Code
+def solution(quiz):
+    answer = []
+    for q in quiz:
+        L,R = q.split(' = ')
+        a,op,b = L.split()
+        if op=='+':
+            result = 'O' if int(a)+int(b)==int(R) else 'X'
+            answer.append(result)
+        else:
+            result = 'O' if int(a)-int(b)==int(R) else 'X'
+            answer.append(result)
+    return answer
+
+
+'''
+    현업에서는 eval이 보안상의 이유 때문에 잘 안 쓰인다고해서 eval을 쓰지 않는 코드를 들고옴.
+'''
