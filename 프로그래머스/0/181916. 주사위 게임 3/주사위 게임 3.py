f99f035ac25b7e305587fa485fb3292608b757ef
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-09-30 00:34:02 +0900

    Update 주사위 게임 3.py

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -24,3 +24,23 @@ def solution(a, b, c, d):
         return q*r
     else: # len(new_dice_list) == 4
         return min(dice_list)
+
+# Another Code
+def solution(a, b, c, d):
+    l = [a,b,c,d]
+    c = [l.count(x) for x in l]
+    if max(c) == 4:
+        return 1111*a
+    elif max(c) == 3:
+        return (10*l[c.index(3)]+l[c.index(1)])**2
+    elif max(c) == 2:
+        if min(c) == 1:
+            return eval('*'.join([str(l[i]) for i, x in enumerate(c) if x == 1]))
+        else:
+            return (max(l) + min(l)) * abs(max(l) - min(l))
+    else:
+        return min(l)
+
+'''
+    사실 이게 조금 더 간단해 보이긴 한다 ㅎㅎ,,
+'''
