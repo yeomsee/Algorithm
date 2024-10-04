e93ffa64b7a9ab3efc6ecee2edf103ae2d582fdc
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-10-05 01:10:02 +0900

    Update 문자열 내 p와 y의 개수.py

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -5,3 +5,7 @@ def solution(string):
     if (string.count('p') == 0) and (string.count('y') == 0):
         return True
     return string.count('p') == string.count('y')
+
+# Simpler Code
+def solution(string):
+    return string.lower().count('p') == string.lower().count('y')
