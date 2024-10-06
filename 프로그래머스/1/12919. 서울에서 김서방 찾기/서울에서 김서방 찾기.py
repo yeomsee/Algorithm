0d8a89e703138c3a4731dfeb34050dfa7652ed9a
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-10-06 19:45:29 +0900

    Update 서울에서 김서방 찾기.py

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -3,3 +3,7 @@ def solution(seoul):
     for i, p in enumerate(seoul):
         if p == "Kim":
             return f"김서방은 {i}에 있다"
+
+# Best Code
+def findKim(seoul):
+    return "김서방은 {}에 있다".format(seoul.index('Kim'))
