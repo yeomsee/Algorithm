c5cd8a5e85df4ec42bd80004110c84b0eb079c41
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-10-07 11:08:38 +0900

    Update 가운데 글자 가져오기.py

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -4,3 +4,7 @@ def solution(s):
     if n%2 != 0:
         return s[n//2]
     return s[n//2-1] + s[n//2]
+
+# Simpler Code
+def string_middle(str):
+    return str[(len(str)-1)//2 : len(str)//2 + 1]
