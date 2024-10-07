14847be5470d9ff9351cd7db02526cba47413260
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-10-07 10:48:21 +0900

    Update 핸드폰 번호 가리기.py

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -5,3 +5,7 @@ def solution(phone_number):
         answer_string += '*'
     answer_string += phone_number[-4:]
     return answer_string
+
+# Best Code
+def hide_numbers(s):
+    return "*"*(len(s)-4)+s[-4:]
