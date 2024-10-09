5bb596aa5f1b52237bc2e67e719c80908720e097
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-10-09 13:27:24 +0900

    Update 문자열 다루기 기본.py

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,3 +1,7 @@
 # My Code
 def solution(string):
     return (len(string)==4 or len(string)==6) and all(s.isdigit() for s in string)
+
+# Simpler Code
+def alpha_string46(s):
+    return s.isdigit() and len(s) in [4,6]
