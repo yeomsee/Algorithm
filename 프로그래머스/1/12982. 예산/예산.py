8db8ff5bb1a173c4eda69ae54d2aebc82e7f61d8
yeom._.see <114807871+yeomsee@users.noreply.github.com>
2024-10-19 19:52:28 +0900

    Update 예산.py

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -9,3 +9,10 @@ def solution(d, budget):
             budget -= i
             
     return count
+
+# Best Code
+def solution(d, budget):
+    d.sort()
+    while budget < sum(d):
+        d.pop()
+    return len(d)
